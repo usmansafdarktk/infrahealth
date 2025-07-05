2eeff643d690455fde8826942ef67316e908ac66
usmansafdarktk <usmansafderktk@gmail.com>
2025-07-05 16:25:54 +0500

    Added prometheus_client to dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="infrahealth",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["click", "psutil", "docker", "requests"],
+    install_requires=["click", "psutil", "docker",
+                      "requests", "prometheus_client"],
     entry_points={
         "console_scripts": [
             "infrahealth = infrahealth.cli:main"
