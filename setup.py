0d42ab5e714239420727827a5afd008c74104bb5
usmansafdarktk <usmansafderktk@gmail.com>
2025-07-05 15:26:36 +0500

    Added docker library to dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="infrahealth",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["click", "psutil", "docker"],
+    entry_points={
+        "console_scripts": [
+            "infrahealth = infrahealth.cli:main"
+        ]
+    },
+    author="Usman Safder"
+    description="A CLI for infrastructure health monitoring",
+)
