dcbad3a2223162649d373a3464afc5fffe4ed03f
usmansafdarktk <usmansafderktk@gmail.com>
2025-07-06 12:00:50 +0500

    updated setup.py for building it as a package

--- setup.py
+++ setup.py
@@ -2,15 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="infrahealth",
-    version="0.1.0",
+    version="0.1.0",  # Update this for each release
     packages=find_packages(),
     install_requires=["click", "psutil", "docker",
                       "requests", "prometheus_client"],
     entry_points={
         "console_scripts": [
-            "infrahealth = infrahealth.cli:main"
-        ]
+            "infrahealth=infrahealth.cli:cli",
+        ],
     },
-    author="Usman Safder",
-    description="A CLI for infrastructure health monitoring",
+    author="Muhammad Usman Safder",
+    author_email="usmansafderktk@gmail.com",
+    description="A CLI for monitoring server and Docker container health",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/usmansafdarktk/infrahealth.git",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
